eaf4302412df14456eccbdbfdfdc2519d539cc84
farinola <fp.arinola@gmail.com>
2021-03-11 11:15:45 +0000

    show decrypted password on the credentials edit modal

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controllers/HomeController.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controllers/HomeController.java
@@ -3,9 +3,8 @@ package com.udacity.jwdnd.course1.cloudstorage.controllers;
 import com.udacity.jwdnd.course1.cloudstorage.models.Credential;
 import com.udacity.jwdnd.course1.cloudstorage.models.File;
 import com.udacity.jwdnd.course1.cloudstorage.models.Note;
-import com.udacity.jwdnd.course1.cloudstorage.services.CredentialService;
-import com.udacity.jwdnd.course1.cloudstorage.services.FileService;
-import com.udacity.jwdnd.course1.cloudstorage.services.NoteService;
+import com.udacity.jwdnd.course1.cloudstorage.models.User;
+import com.udacity.jwdnd.course1.cloudstorage.services.*;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -19,13 +18,19 @@ public class HomeController {
     private final CredentialService credentialService;
     private final FileService fileService;
     private final NoteService noteService;
+    private final EncryptionService encryptionService;
+    private final UserService userService;
 
     public HomeController(CredentialService credentialService,
                           FileService fileService,
-                          NoteService noteService) {
+                          NoteService noteService,
+                          EncryptionService encryptionService,
+                          UserService userService) {
         this.credentialService = credentialService;
         this.fileService = fileService;
         this.noteService = noteService;
+        this.encryptionService = encryptionService;
+        this.userService = userService;
     }
 
     @ModelAttribute("storedCredentials")
@@ -43,9 +48,19 @@ public class HomeController {
         return this.fileService.getAllFiles();
     }
 
+    @ModelAttribute("decryptor")
+    public ICanDecryptPassword decryptor() {
+        User activeUser = this.userService.getActiveUser();
+        return password -> this.encryptionService.decryptValue(password, activeUser.getSalt());
+    }
+
     @GetMapping
     public String home(@ModelAttribute("noteForm") Note noteForm,
                        @ModelAttribute("credentialForm") Credential credentialForm) {
         return "home";
+    }
+
+    interface ICanDecryptPassword {
+        String decrypt(String password);
     }
 }
